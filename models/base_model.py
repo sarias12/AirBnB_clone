5deaf36f9ae0ebdf5755d1ba04bd6f2a23e80020
LizethVictoria20 <lizvictoria754@gmail.com>
2020-06-26 03:02:48 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,21 @@ import datetime
 class BaseModel:
     '''[Defines all common attributes/methods for other classes]
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''[Constructor]
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ['created_at']:
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key in ['updated_at']:
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key not in ['__class__']:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''[Function str]
