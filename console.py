52eb2ddc646c999267d930e479555c1f4052fe29
sarias12 <1638@holbertonschool.com>
2020-06-26 21:47:39 +0000

    Update Console

--- console.py
+++ console.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
-'''[summary]
-'''
+"""
+The command interpreter
+"""
 import cmd
 import sys
 
 
 class HBNBCommand(cmd.Cmd):
-
+    """
+    HBNBCommand
+    """
     prompt = '(hbnb) '
 
+    def do_EOF(self, line):
+        """
+        Ctrl + D command to exit the program (End of File).
+        """
+        print()
+        return True
+
+    def do_quit(self, args):
+        """
+        Quit command to exit the program.
+        """
+        print()
+        return True
+
     def emptyline(self):
-        '''[summary]
-        '''
+        """
+        This does nothing. An empty line + ENTER shouldn’t execute anything.
+        """
         pass
 
-    def do_quit(self, list):
-        '''Quit command to exit the program
-        '''
-        print()
-        return True
+    def do_create(self, name):
+        pass
 
-    def do_EOF(self, line):
-        '''[summary]
-        '''
-        print()
-        return True
+    def do_show(self, args):
+        pass
+
+    def do_destroy(self, args):
+        pass
+
+    def do_all(self, args):
+        pass
+
+    def do_update(self, args):
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
