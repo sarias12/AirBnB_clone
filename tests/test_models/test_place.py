3648e7afffaa03121bd6f447d5cb866a130bbd9c
Sergio Steben Arias Quintero <60366851+sarias12@users.noreply.github.com>
2020-07-01 15:37:45 -0500

    Revert "Update Unit Test for class"

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -3,7 +3,6 @@ Test File for Place
 """
 import unittest
 import pep8
-from models.place import Place
 
 
 class tests_Place(unittest.TestCase):
@@ -15,24 +14,3 @@ class tests_Place(unittest.TestCase):
         result = pep8style.check_files(['models/place.py'])
         self.assertEqual(
             result.total_errors, 0, "Found code style errors (and warnings).")
-
-    def test_args(self):
-            """[Unittest for class Place]
-            """
-            name_class_bas = Place(name="text")
-            self.assertEqual(type(name_class_bas).__name__(Place))
-            self.assertTrue(hasattr(name_class_bas="created_at"))
-            self.assertTrue(hasattr(name_class_bas="updated_at"))
-            self.assertTrue(hasattr(name_class_bas="__class__"))
-            self.assertTrue(hasattr(name_class_bas="id"))
-            self.assertTrue(hasattr(name_class_bas="city_id"))
-            self.assertTrue(hasattr(name_class_bas="user_id"))
-            self.assertTrue(hasattr(name_class_bas="name"))
-            self.assertTrue(hasattr(name_class_bas="description"))
-            self.assertTrue(hasattr(name_class_bas="number_rooms"))
-            self.assertTrue(hasattr(name_class_bas="number_bathrooms"))
-            self.assertTrue(hasattr(name_class_bas="max_guest"))
-            self.assertTrue(hasattr(name_class_bas="price_by_night"))
-            self.assertTrue(hasattr(name_class_bas="latitude"))
-            self.assertTrue(hasattr(name_class_bas="longitude"))
-            self.assertTrue(hasattr(name_class_bas="amenity_ids"))
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,7 +3,6 @@ Test File for User
 """
 import unittest
 import pep8
-from models.user import User
 
 
 class tests_User(unittest.TestCase):
@@ -15,17 +14,3 @@ class tests_User(unittest.TestCase):
         result = pep8style.check_files(['models/user.py'])
         self.assertEqual(
             result.total_errors, 0, "Found code style errors (and warnings).")
-
-    def test_args(self):
-        """[Unittest for class User]
-        """
-        name_class_bas = User(name="text")
-        self.assertEqual(type(name_class_bas).__name__(User))
-        self.assertTrue(hasattr(name_class_bas="created_at"))
-        self.assertTrue(hasattr(name_class_bas="updated_at"))
-        self.assertTrue(hasattr(name_class_bas="__class__"))
-        self.assertTrue(hasattr(name_class_bas="id"))
-        self.assertTrue(hasattr(name_class_bas="email"))
-        self.assertTrue(hasattr(name_class_bas="password"))
-        self.assertTrue(hasattr(name_class_bas="first_name"))
-        self.assertTrue(hasattr(name_class_bas="last_name"))
