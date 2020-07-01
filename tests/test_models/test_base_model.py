5862e947c8d9881b50b52d3ea96b2ce69da36218
LizethVictoria20 <lizvictoria754@gmail.com>
2020-07-01 14:15:03 +0000

    Add new test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,3 +15,12 @@ class tests_BaseModel(unittest.TestCase):
         result = pep8style.check_files(['models/base_model.py'])
         self.assertEqual(
             result.total_errors, 0, "Found code style errors (and warnings).")
+
+
+    def test_args(self):
+        name_class_bas = BaseModel(name="Holis")
+        self.assertEqual(type(name_class_bas).__name__(BaseModel))
+        self.assertFalse(hasattr(name_class_bas="created_at"))
+        self.assertFalse(hasattr(name_class_bas="updated_at"))
+        self.assertFalse(hasattr(name_class_bas="__class__"))
+        self.assertFalse(hasattr(name_class_bas="id"))
