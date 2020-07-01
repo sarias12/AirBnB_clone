67a0b3ff49fda02b1ee4c130f28dc2d8f233e212
sarias12 <1638@holbertonschool.com>
2020-07-01 20:04:32 +0000

    Update Unit Test for class

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -15,3 +15,15 @@ class tests_City(unittest.TestCase):
         result = pep8style.check_files(['models/city.py'])
         self.assertEqual(
             result.total_errors, 0, "Found code style errors (and warnings).")
+
+    def test_args(self):
+        """[Unittest for class City]
+        """
+        name_class_bas = City(name="text")
+        self.assertEqual(type(name_class_bas).__name__(City))
+        self.assertTrue(hasattr(name_class_bas="created_at"))
+        self.assertTrue(hasattr(name_class_bas="updated_at"))
+        self.assertTrue(hasattr(name_class_bas="__class__"))
+        self.assertTrue(hasattr(name_class_bas="id"))
+        self.assertTrue(hasattr(name_class_bas="state_id"))
+        self.assertTrue(hasattr(name_class_bas="name"))
